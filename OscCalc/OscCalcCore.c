ce4832018c08618f901f89bf74852d253bffb081
Gabriel Perdue <perdue@fnal.gov>
2013-09-27 16:40:36 -0500

    Add a bunch of infrastructure functions for computing oscillation probabilities.

--- OscCalc/OscCalcCore.c
+++ OscCalc/OscCalcCore.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
 #include "OscCalcCore.h"
 
 void printReferences() 
@@ -76,24 +77,210 @@ struct nuOscParams * create_default_nuOscParams()
   return params;
 }
 
-
-double getMass12Splitting( const struct nuOscParams * params )
+void invert_hierarchy( struct nuOscParams * params )
 {
-  return params->delta_m12_squared;
+  assert(0 != params->hierarchy);
+  params->hierarchy = -params->hierarchy;
+  params->delta_m23_squared = -params->delta_m23_squared;
+  params->delta_m13_squared = -params->delta_m13_squared;
+  params->delta_m32_squared = -params->delta_m32_squared;
+  params->delta_m31_squared = -params->delta_m31_squared;
 }
 
-double getMass23Splitting( const struct nuOscParams * params )
-{
-  double massSplitting = 0.0;
-  /*
-     static const double default_dm23_squared_inverted =  2.43e-3;
-     static const double default_dm32_squared_inverted = -2.43e-3;
-     if (params->helicity < 0) {
-     massSplitting = params->
-     } 
-     else {
-
-     }
-     */
-  return massSplitting;
+double SinSqrd2Theta12( const struct nuOscParams * pp ) { 
+  double Theta12 = pp->theta12;
+  return sin( 2 * Theta12 ) * sin( 2 * Theta12 ); 
 }
+
+double SinSqrd2Theta13( const struct nuOscParams * pp ) { 
+  double Theta13 = pp->theta13;
+  return sin( 2 * Theta13 ) * sin( 2 * Theta13 ); 
+}
+
+double SinSqrd2Theta23( const struct nuOscParams * pp ) { 
+  double Theta23 = pp->theta23;
+  return sin( 2 * Theta23 ) * sin( 2 * Theta23 ); 
+}
+
+double SinSqrdTheta23( const struct nuOscParams * pp )  { 
+  double Theta23 = pp->theta23;
+  return sin( Theta23 ) * sin( Theta23 ); 
+}
+
+double CosSqrdTheta23( const struct nuOscParams * pp )  { 
+  double Theta23 = pp->theta23;
+  return cos( Theta23 ) * cos( Theta23 ); 
+}
+
+/* L in km, E in GeV */
+/* double Delta12( const struct nuOscParams * pp, double L, double E ) { */ 
+/*   return L_OVER_E_CONVERTER * pp->delta_m12_squared * (L / E); */ 
+/* } */
+
+/* double Delta13( const struct nuOscParams * pp, double L, double E ) { */ 
+/*   return L_OVER_E_CONVERTER * pp->delta_m13_squared * (L / E); */ 
+/* } */
+
+/* double Delta23( const struct nuOscParams * pp, double L, double E ) { */ 
+/*   return L_OVER_E_CONVERTER * pp->delta_m23_squared * (L / E); */ 
+/* } */
+
+/* double Delta21( const struct nuOscParams * pp, double L, double E ) { */ 
+/*   return L_OVER_E_CONVERTER * pp->delta_m21_squared * (L / E); */ 
+/* } */
+
+/* double Delta31( const struct nuOscParams * pp, double L, double E ) { */ 
+/*   return L_OVER_E_CONVERTER * pp->delta_m31_squared * (L / E); */ 
+/* } */
+
+/* double Delta32( const struct nuOscParams * pp, double L, double E ) { */ 
+/*   return L_OVER_E_CONVERTER * pp->delta_m32_squared * (L / E); */ 
+/* } */
+
+/* L in km, E in GeV */
+double Delta12( const struct nuOscParams * pp, double LovrE ) { 
+  return L_OVER_E_CONVERTER * pp->delta_m12_squared * LovrE; 
+}
+
+double Delta13( const struct nuOscParams * pp, double LovrE ) { 
+  return L_OVER_E_CONVERTER * pp->delta_m13_squared * LovrE; 
+}
+
+double Delta23( const struct nuOscParams * pp, double LovrE ) { 
+  return L_OVER_E_CONVERTER * pp->delta_m23_squared * LovrE; 
+}
+
+double Delta21( const struct nuOscParams * pp, double LovrE ) { 
+  return L_OVER_E_CONVERTER * pp->delta_m21_squared * LovrE; 
+}
+
+double Delta31( const struct nuOscParams * pp, double LovrE ) { 
+  return L_OVER_E_CONVERTER * pp->delta_m31_squared * LovrE; 
+}
+
+double Delta32( const struct nuOscParams * pp, double LovrE ) { 
+  return L_OVER_E_CONVERTER * pp->delta_m32_squared * LovrE; 
+}
+
+/* Convenience Functions for Sines and Cosines of mixing angles & \delta_{CP}. */
+double  s12( const struct nuOscParams * pp ) { 
+  return sin( pp->theta12 ); 
+}
+
+double  s13( const struct nuOscParams * pp ) { 
+  return sin( pp->theta13 ); 
+}
+
+double  s23( const struct nuOscParams * pp ) { 
+  return sin( pp->theta23 ); 
+}
+
+double  c12( const struct nuOscParams * pp ) { 
+  return cos( pp->theta12 ); 
+}
+
+double  c13( const struct nuOscParams * pp ) { 
+  return cos( pp->theta13 ); 
+}
+
+double  c23( const struct nuOscParams * pp ) { 
+  return cos( pp->theta23 ); 
+}
+
+double cdlt( const struct nuOscParams * pp ) { 
+  return cos( pp->deltaCP ); 
+}
+
+double sdlt( const struct nuOscParams * pp ) { 
+  return sin( pp->deltaCP ); 
+}
+
+#define S12 s12(pp)
+#define S13 s13(pp)
+#define S23 s23(pp)
+#define C12 c12(pp)
+#define C13 c13(pp)
+#define C23 c23(pp)
+#define CDLT cdlt(pp)
+#define SDLT sdlt(pp)
+double complex Ufm( const struct nuOscParams * pp, int flavor, int mass )
+{
+   /* |   e1   e2   e3 | */
+   /* |  mu1  mu2  mu3 | */
+   /* | tau1 tau2 tau3 | */
+  double real = 0.;
+  double im   = 0.;
+  if( 1==flavor && 1==mass ) { // e1
+    real = C13*C12;
+  }
+  else if( 2==flavor && 1==mass ) { // mu1 
+    real = -C23*S12 - S13*C12*S23*CDLT;
+    im   = -S13*C12*S23*SDLT;
+  }
+  else if( 3==flavor && 1==mass ) { // tau1 
+    real = S23*S12 - S13*C12*C23*CDLT;
+    im   = -S13*C12*C23*SDLT;
+  }
+  else if( 1==flavor && 2==mass ) { // e2
+    real = C13*S12;
+  }
+  else if( 2==flavor && 2==mass ) { // mu2
+    real = C23*C12 - S13*S12*S23*CDLT;
+    im   = -S13*S12*S23*SDLT;
+  }
+  else if( 3==flavor && 2==mass ) { // tau2
+    real = -S23*C12 - S13*S12*C23*CDLT;
+    im   = -S13*S12*C23*SDLT;
+  }
+  else if( 1==flavor && 3==mass ) { // e3
+    real = S13*CDLT;
+    im   = -S13*SDLT;
+  }
+  else if( 2==flavor && 3==mass ) { // mu3
+    real = C13*S23;
+  }
+  else if( 3==flavor && 3==mass ) { // tau3
+    real = C13*C23;
+  }
+  double complex retval = real +  im*I;
+  return retval;
+
+}
+
+double Ue1Sqrd( const struct nuOscParams * pp )   { 
+  return abs( Ufm(pp,1,1)*conj(Ufm(pp,1,1)) ); 
+}
+
+double Ue2Sqrd( const struct nuOscParams * pp )   { 
+  return abs( Ufm(pp,1,2)*conj(Ufm(pp,1,2)) ); 
+}
+
+double Ue3Sqrd( const struct nuOscParams * pp )   { 
+  return abs( Ufm(pp,1,3)*conj(Ufm(pp,1,3)) ); 
+}
+
+double Umu1Sqrd( const struct nuOscParams * pp )  { 
+  return abs( Ufm(pp,2,1)*conj(Ufm(pp,2,1)) ); 
+}
+
+double Umu2Sqrd( const struct nuOscParams * pp )  { 
+  return abs( Ufm(pp,2,2)*conj(Ufm(pp,2,2)) ); 
+}
+
+double Umu3Sqrd( const struct nuOscParams * pp )  { 
+  return abs( Ufm(pp,2,3)*conj(Ufm(pp,2,3)) ); 
+}
+
+double Utau1Sqrd( const struct nuOscParams * pp ) { 
+  return abs( Ufm(pp,3,1)*conj(Ufm(pp,3,1)) ); 
+}
+
+double Utau2Sqrd( const struct nuOscParams * pp ) { 
+  return abs( Ufm(pp,3,2)*conj(Ufm(pp,3,2)) ); 
+}
+
+double Utau3Sqrd( const struct nuOscParams * pp ) { 
+  return abs( Ufm(pp,3,3)*conj(Ufm(pp,3,3)) ); 
+}
+
+
--- OscCalc/OscCalcCore.h
+++ OscCalc/OscCalcCore.h
@@ -1,3 +1,4 @@
+#include <complex.h>
 
 const double L_OVER_E_CONVERTER = 1.2669;
 
@@ -18,31 +19,40 @@ const char* globalFitsRef     =
 	"arXiv 1209.3023";
 
 /* Current Ref: 1209.3023 */
-const int    default_helicity   = 1;     /* Normal */
-const int    default_hierarchy  = 1;     /* Nu */
+const int    default_helicity   = 1;     /* Nu, negative is Antinu */
+const int    default_hierarchy  = 1;     /* Normal, negatis is Inverted */
 const double default_deltaCP    = 0.0;
 
 /* 1-2 Splitting */
-const double default_dm12_squared          = -7.50e-5;
-const double default_dm21_squared          =  7.50e-5;
+#define DEFDM12 -7.50e-5
+const double default_dm12_squared          =      DEFDM12;
+const double default_dm21_squared          = -1.0*DEFDM12;
 
 /* Normal Hierarchy Mass Splittings */
-const double default_dm31_squared_normal   =    2.47e-3;
-const double default_dm32_squared_normal   =    2.47e-3 - 7.50e-5;
-const double default_dm13_squared_normal   =   -2.47e-3;
-const double default_dm23_squared_normal   =  -(2.47e-3 - 7.50e-5);
+#define DEFDM31 2.47e-3
+const double default_dm31_squared_normal   =      DEFDM31;
+const double default_dm32_squared_normal   =      DEFDM31 + DEFDM12;
+const double default_dm13_squared_normal   = -1.0*DEFDM31;
+const double default_dm23_squared_normal   = -1.0*(DEFDM31 + DEFDM12);
 
 /* Inverted Hierarchy Splittings */
-const double default_dm32_squared_inverted =  -2.43e-3;
-const double default_dm23_squared_inverted =   2.43e-3;
-const double default_dm13_squared_inverted =   2.43e-3 - 7.50e-5;
-const double default_dm31_squared_inverted = -(2.43e-3 - 7.50e-5);
+#define DEFDM32 -2.43e-3
+const double default_dm32_squared_inverted =        DEFDM32;
+const double default_dm23_squared_inverted =   -1.0*DEFDM32;
+const double default_dm13_squared_inverted =   -1.0*DEFDM32 + DEFDM12;
+const double default_dm31_squared_inverted = -1.0*(-1.0*DEFDM32 + DEFDM12);
 
 /* Mixing Angles */
-const double default_theta12  = 33.3 * 3.14159 / 180;
-const double default_theta23  = 40.0 * 3.14159 / 180;
-const double default_theta13  = 8.6 * 3.14159 / 180;
-const double default_baseline = 735.0; /* km */
+#define THETA12DEG 33.3
+#define THETA23DEG 40.0
+#define THETA13DEG 8.6
+#define DEGTORAD 3.14159 / 180;
+const double default_theta12  = THETA12DEG * DEGTORAD;
+const double default_theta23  = THETA23DEG * DEGTORAD;
+const double default_theta13  = THETA13DEG * DEGTORAD;
+
+#define FNALTOSOUDAN 735.0 /* km */
+const double default_baseline = FNALTOSOUDAN; /* km */
 
 /* Default Experimental Parameters */
 const double default_energy   = 2.0;   /* GeV */
@@ -75,5 +85,49 @@ void printDefaultParameters();
 
 struct nuOscParams * create_default_nuOscParams();
 
-double getMass12Splitting( const struct nuOscParams * params );
-double getMass23Splitting( const struct nuOscParams * params );
+void invert_hierarchy( struct nuOscParams * params );
+
+double SinSqrd2Theta12( const struct nuOscParams * pp );
+double SinSqrd2Theta13( const struct nuOscParams * pp );
+double SinSqrd2Theta23( const struct nuOscParams * pp );
+double SinSqrdTheta23( const struct nuOscParams * pp );
+double CosSqrdTheta23( const struct nuOscParams * pp );
+
+/* L in km, E in GeV */
+/* double Delta12( const struct nuOscParams * pp, double L, double E ); */
+/* double Delta13( const struct nuOscParams * pp, double L, double E ); */
+/* double Delta23( const struct nuOscParams * pp, double L, double E ); */
+/* double Delta21( const struct nuOscParams * pp, double L, double E ); */
+/* double Delta31( const struct nuOscParams * pp, double L, double E ); */
+/* double Delta32( const struct nuOscParams * pp, double L, double E ); */
+
+/* L in km, E in GeV, LovrE = L/E */
+double Delta12( const struct nuOscParams * pp, double LovrE );
+double Delta13( const struct nuOscParams * pp, double LovrE );
+double Delta23( const struct nuOscParams * pp, double LovrE );
+double Delta21( const struct nuOscParams * pp, double LovrE );
+double Delta31( const struct nuOscParams * pp, double LovrE );
+double Delta32( const struct nuOscParams * pp, double LovrE );
+
+/* Convenience Functions for Sines and Cosines of mixing angles & \delta_{CP}. */
+double  s12( const struct nuOscParams * pp ); 
+double  s13( const struct nuOscParams * pp ); 
+double  s23( const struct nuOscParams * pp ); 
+double  c12( const struct nuOscParams * pp ); 
+double  c13( const struct nuOscParams * pp ); 
+double  c23( const struct nuOscParams * pp ); 
+double cdlt( const struct nuOscParams * pp ); 
+double sdlt( const struct nuOscParams * pp ); 
+
+double complex Ufm( const struct nuOscParams * pp, int flavor, int mass );
+
+double Ue1Sqrd( const struct nuOscParams * pp );   
+double Ue2Sqrd( const struct nuOscParams * pp );   
+double Ue3Sqrd( const struct nuOscParams * pp );   
+double Umu1Sqrd( const struct nuOscParams * pp );  
+double Umu2Sqrd( const struct nuOscParams * pp );  
+double Umu3Sqrd( const struct nuOscParams * pp );  
+double Utau1Sqrd( const struct nuOscParams * pp ); 
+double Utau2Sqrd( const struct nuOscParams * pp ); 
+double Utau3Sqrd( const struct nuOscParams * pp ); 
+
