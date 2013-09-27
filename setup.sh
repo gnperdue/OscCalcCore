77b3a9253f09ee4f2a504626ed85ffbe792f7846
Gabriel Perdue <perdue@fnal.gov>
2013-09-27 14:02:03 -0500

    Modify ld/dyld path based on architecture.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-pushd .. >& /dev/null
-export DYLD_LIBRARY_PATH=`pwd`/lib:$DYLD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-popd >& /dev/null
 
+ARCHIT=`uname`
+
+if [ $ARCHIT = 'Darwin' ]
+then
+  export DYLD_LIBRARY_PATH=`pwd`/lib:$DYLD_LIBRARY_PATH
+else
+  export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+fi
+
+unset ARCHIT
