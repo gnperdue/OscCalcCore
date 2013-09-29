e3c8ef5e977c739fd8c12c851afc4350b6dc5022
Gabriel Perdue <gnperdue@me.com>
2013-09-29 05:52:03 -0500

    Call a library function and print plausible-looking numbers.

--- test/OscCalcTester.c
+++ test/OscCalcTester.c
@@ -5,7 +5,22 @@
 
 int main( int argc, char* argv[] ) 
 {
+  const int nenergies = 100;
+  const double baseline = FNAL_TO_SOUDAN;
+
   printDefaultParameters();
+
+  struct nuOscParams *params = create_default_nuOscParams();
+
+  double energies[nenergies];
+  double probabilities[nenergies];
+  for (int i = 0; i < nenergies; ++i) {
+    energies[i] = i * 0.1 + 0.1;
+    probabilities[i] = twoFlavorMuSurvive(params, energies[i], baseline);
+    printf("%0.2f  %0.5f\n", energies[i], probabilities[i]);
+  }
+
+  free(params);
 
   return 0;
 }
