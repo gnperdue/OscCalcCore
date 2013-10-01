9129878cdf38ea1cd1e5dcd6fab41f24ce17e73c
Gabriel Perdue <gnperdue@me.com>
2013-10-01 05:08:24 -0500

    Import another function into the R-API.

--- OscCalc/OscCalcR.c
+++ OscCalc/OscCalcR.c
@@ -18,4 +18,22 @@ void twoFlavorMuSurviveArray_R(
   free(params);
 }
 
+void threeFlavorMuSurvive_R( 
+    double *baseline,
+    double *deltaCP,
+    double *theta23,
+    int    *nenergies,
+    double energies[],
+    double probabilities[])
+{
+  struct nuOscParams *params = create_default_nuOscParams();
+  params->deltaCP = *deltaCP;
+  params->theta23 = *theta23;
+  for (int i = 0; i<(*nenergies); ++i) {
+    probabilities[i] = threeFlavorMuSurvive( params, energies[i], *baseline ); 
+  }
+  free(params);
+}
+
+
 #endif
--- OscCalc/OscCalcR.h
+++ OscCalc/OscCalcR.h
@@ -15,4 +15,21 @@ void twoFlavorMuSurviveArray_R(
     double energies[],
     double probabilities[]);
 
+/*! Survivial probability for muon-flavor neutrinos in a three-flavor framework.
+  \param baseline      Input
+  \param deltaCP       Input
+  \param theta23       Input : Study the octant of theta23.
+  \param nenergies     Input : The length of the energies array and the probabilities array.
+  \param energies      Input : Array of energies.
+  \param probabilities Output: Array of calculated probabilies.
+  */
+void threeFlavorMuSurvive_R( 
+    double *baseline,
+    double *deltaCP,
+    double *theta23,
+    int    *nenergies,
+    double energies[],
+    double probabilities[]);
+
+
 #endif
